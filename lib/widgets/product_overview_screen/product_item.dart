343f6d2f48400cc1c7305ee8f8fa78ea4e3fb817
Uday Menon <udscrick@gmail.com>
2021-05-09 20:34:38 +0530

    Snackbars Added

--- lib/widgets/product_overview_screen/product_item.dart
+++ lib/widgets/product_overview_screen/product_item.dart
@@ -13,40 +13,58 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productinfo = Provider.of<Product>(context,listen: false);//listen is set to false as we dont want the entire widget to rebuild but only the part inside consumer
-    final cartprovider = Provider.of<Cart>(context,listen: false);//We dont need to rerender the product item whenever the cart changes, so listen is false
+    final productinfo = Provider.of<Product>(context,
+        listen:
+            false); //listen is set to false as we dont want the entire widget to rebuild but only the part inside consumer
+    final cartprovider = Provider.of<Cart>(context,
+        listen:
+            false); //We dont need to rerender the product item whenever the cart changes, so listen is false
     print("Product Item rebuilds");
     return ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: GridTile(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
         child: GestureDetector(
-          onTap: (){
-            Navigator.of(context).pushNamed('product-details',arguments: productinfo.id);
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed('product-details', arguments: productinfo.id);
           },
-                  child: Image.network(
+          child: Image.network(
             productinfo.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           leading: Consumer<Product>(
-            builder: (ctx,product,child)=>IconButton(icon: Icon(Icons.favorite),
-            onPressed: (){
-              Provider.of<Product>(context,listen: false).markAsFavourite();
-            },
-            color:productinfo.isFavourite?Theme.of(context).accentColor:Colors.white),
-                     
+            builder: (ctx, product, child) => IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {
+                  Provider.of<Product>(context, listen: false)
+                      .markAsFavourite();
+                },
+                color: productinfo.isFavourite
+                    ? Theme.of(context).accentColor
+                    : Colors.white),
           ),
           title: Text(
             productinfo.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            
-            icon: Icon(Icons.shopping_cart),onPressed: (){
-              cartprovider.addItem(productinfo.id, productinfo.price, productinfo.title);
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              cartprovider.addItem(
+                  productinfo.id, productinfo.price, productinfo.title);
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text('Item Added to Cart'),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(label: 'UNDO', onPressed: (){
+                    cartprovider.undoAdd(productinfo.id);
+                  }),
+                  ));
             },
-            color:Theme.of(context).accentColor,),
+            color: Theme.of(context).accentColor,
+          ),
           backgroundColor: Colors.black87,
         ),
       ),
